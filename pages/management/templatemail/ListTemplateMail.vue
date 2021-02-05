dadb161a89bd00aea0c8569494a71b53f599a996
ADMIN <huydo164@gmail.com>
2021-02-05 08:25:53 +0700

    update

--- pages/management/templatemail/ListTemplateMail.vue
+++ pages/management/templatemail/ListTemplateMail.vue
@@ -12,13 +12,40 @@
         :fields="fields"
       >
         <template #category="{item}">
-        <td>
-          <CBadge :color="getBadge(item.category)">
-            {{item.category}}
-          </CBadge>
+          <td>
+            <CBadge>{{getBadge(item.category)}}</CBadge>
+          </td>
+        </template>  
+        <template #show_details="{item, index}">
+        <td class="py-2">
+          <CButton
+            color="primary"
+            variant="outline"
+            square
+            size="sm"
+            @click="toggleDetails(item, index)"
+          >
+            {{Boolean(item._toggled) ? 'Hide' : 'Show'}}
+          </CButton>
         </td>
       </template>
-      
+        <template #details="{item}">
+        <CCollapse :show="Boolean(item._toggled)" :duration="collapseDuration">
+          <CCardBody>
+            <CMedia :aside-image-props="{ height: 102 }">
+              <h4>
+                {{item.name}}
+              </h4>
+              <CButton size="sm" color="info" class="">
+                <nuxt-link to="/management/templatemail/AddTemplateMail">Edit</nuxt-link>
+              </CButton>
+              <CButton size="sm" color="danger" class="ml-1" @click="Delete(item.id,index)">
+                Delete
+              </CButton>
+            </CMedia>
+          </CCardBody>
+        </CCollapse>
+      </template>
       </CDataTable>
     </CCardBody>
   </div>
@@ -27,26 +54,31 @@
 import axios from "axios"
 const fields = [
   {key : 'name',label :'Tên'},
-  'category',
-  'content',
-  'created_at'
+  {key: 'category', label: 'Loại mail'},
+  {key: 'content', label: 'Nội dung'},
+  {key: 'created_at', label: 'Ngày tạo'},
+  {
+    key: 'show_details',
+    label: '',
+    _style: 'width:1%',
+    sorter:false,
+    filter:false,
+  }
 ]
 
 export default {
-  
-  watch: {
-  },
+
   name: 'AdvancedTables',
   data() {
     return {
-     
       dataTemplate : [],
       fields:fields,
+      collapseDuration: 0,
+      details: [],
     }
   },
   mounted() {
     this.listData()
-    
   },
   methods: {
     listData: function () {
@@ -55,18 +87,25 @@ export default {
         this.dataTemplate = response.data
       })
     },
+
     getBadge(status) {
-      switch (status) {
-        case '1': return 'Mail cảm ơn'
-        case '2': return 'Mai offer'
-        case '3': return 'Mail phỏng vấn'
-      }
+      if(status ==1) return 'Thư cảm ơn'
+      else if(status == 2) return 'Thư nhận việc'
+      else return 'Thư phỏng vấn'
     },
+
     toggleDetails(item) {
-      this.$set(this.items[item.id], '_toggled', !item._toggled)
+      this.$set(this.dataTemplate[item.id], '_toggled', !item._toggled)
+      console.log(this.dataTemplate[item.id - 2])
       this.collapseDuration = 300
       this.$nextTick(() => { this.collapseDuration = 0 })
-    }
+    },
+
+    Delete(id,index){
+      axios.delete('http://127.0.0.1:8000/api/template/' + id).then((response) => {
+        this.dataTemplate.splice(index,1)
+      })
+    },
   }
 }
 </script>
