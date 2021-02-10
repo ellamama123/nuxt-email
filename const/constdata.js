ba5fe7e149daecd8fe7b91a791cfdec04cfcb23f
ADMIN <huydo164@gmail.com>
2021-02-11 00:35:26 +0700

    update

--- const/constdata.js
+++ const/constdata.js
@@ -1,3 +1,3 @@
-export const LIST_CATEGORY = [{ value: 0, label: 'Mail cảm ơn' }, { value: 1, label: 'Mail phỏng vấn' }, { value: 2, label: 'Mail offer' }]
-export const LIST_POSITION = [{ value: 0, label: 'C#' }, { value: 1, label: 'PHP' }, { value: 2, label: 'Tester' }]
-export const LIST_STATUS = [{ value: 0, label: 'Chưa gửi' }, { value: 1, label: 'Đã gửi' }]
+export const LIST_CATEGORY = ['Mail Cảm ơn', 'Mail phỏng vấn', 'Mail offer']
+export const LIST_POSITION = ['Thực tập', 'Học việc']
+export const LIST_STATUS = ['Chưa gửi', 'Đã gửi']
--- layouts/containers/_nav.js
+++ layouts/containers/_nav.js
@@ -39,20 +39,20 @@ export default [{
         {
             _name: 'CSidebarNavItem',
             name: 'Quản lý Template mail',
-            route: '/management/templatemail/',
-            to: '/management/templatemail/'
+            route: '/management/templatemail/ListTemplateMail',
+            to: '/management/templatemail/ListTemplateMail'
         },
         {
             _name: 'CSidebarNavItem',
             name: 'Quản lý lịch sử gửi mail',
-            route: '/management/history/',
-            to: '/management/history/'
+            route: '/management/history/ListHistoryMail',
+            to: '/management/history/ListHistoryMail'
         },
         {
             _name: 'CSidebarNavItem',
             name: 'Quản lý ứng viên',
-            route: '/management/candidate/',
-            to: '/management/candidate/'
+            route: '/management/candidate/ListCandidate',
+            to: '/management/candidate/ListCandidate'
         },
         {
             _name: 'CSidebarNavTitle',
