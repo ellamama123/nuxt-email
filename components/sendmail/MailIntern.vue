2318971518b0d64c82d805f3a970f092325b53be
ADMIN <huydo164@gmail.com>
2021-02-24 16:20:53 +0700

    update

--- components/sendmail/MailIntern.vue
+++ components/sendmail/MailIntern.vue
@@ -157,12 +157,8 @@ export default {
         value["candidate_email"] = value.email;
         value["candidate_id"] = value.id;
         value["status"] = value.status;
-        value["content"] = this.changeText(
-          this.getContentMailIntern(value.category_mail),
-          value["name"],
-          value["dateTime"],
-          this.getPosition(value["position"])
-        );
+        value["position_name"] = this.getPosition(value.position)
+        value["content"] = this.getContentMailIntern(value.category_mail)
         value["content"] = value["content"].replace(/\n/gi, "\n");
         value["datetime_interview"] = value.dateTime;
 
@@ -193,20 +189,6 @@ export default {
       }
     },
 
-    changeText: function(content, name, dateTime, position) {
-      if (!dateTime) {
-        return content;
-      } else {
-        dateTime = moment(String(dateTime)).format("DD/MM/YYYY hh:mm");
-        content = content
-          .replace("[Name]", name)
-          .replace("[dateTime]", dateTime)
-          .replace("[Position]", position);
-        console.log(content);
-        return content;
-      }
-    },
-
     getStatus(status) {
       for (const sta of this.LIST_STATUS) {
         if (status == sta.value) {
@@ -221,17 +203,16 @@ export default {
     },
 
     showModal(item) {
-      this.dataSend.forEach((element) => {
-        if (item.id == element.id) {
-          this.content = this.changeText(
-            this.getContentMailIntern(element.category_mail),
-            item.name,
-            item.dateTime,
-            item.position
-          );
+      console.log(this.dataSend)
+      this.dataSend.find((element) => {
+        if(element.id === item.id){
+          axios.get('http://127.0.0.1:8000/api/previewMail?id=' +  item.category_mail).then((response) => {
+            this.content = response.data
+            this.content = this.content.replace("[Name]", element.name).replace("[dateTime]", this.convertDate(element.dateTime)).replace("[Position]", this.getPosition(element.position));
+          });
           this.warningModal = true;
         }
-      });
+      })
     },
 
     getContentMailIntern(id) {
