f6be715d6d90c71103235070fa9ea1d3dda23c1f
Nguyễn Đình Chiến <nguyendinhchienpc@gmail.com>
2021-02-25 17:06:54 +0700

    update

--- .nuxt/auth/plugin.js
+++ .nuxt/auth/plugin.js
@@ -4,6 +4,7 @@ import './middleware'
 
 // Active schemes
 import scheme_3e2123be from './schemes/local.js'
+import scheme_6ce4fcca from './schemes/oauth2.js'
 
 export default function (ctx, inject) {
   // Options
@@ -15,6 +16,9 @@ export default function (ctx, inject) {
   // Register strategies
   // local
   $auth.registerStrategy('local', new scheme_3e2123be($auth, {"endpoints":{"login":{"url":"/login","method":"post","propertyName":"meta.token"},"logout":false,"user":{"url":"/user","method":"post","propertyName":"data"}},"_name":"local"}))
+
+  // google
+  $auth.registerStrategy('google', new scheme_6ce4fcca($auth, {"client_id":"1038999090260-j6vqc9ocqqelsaop901obbcdfd7n4h6q.apps.googleusercontent.com","_name":"google","authorization_endpoint":"https://accounts.google.com/o/oauth2/auth","userinfo_endpoint":"https://www.googleapis.com/oauth2/v3/userinfo","scope":["openid","profile","email"]}))
 
   // Inject it to nuxt context as $auth
   inject('auth', $auth)
