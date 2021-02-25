d2cee1751c40927f687d1c64d4188367d5ca57c2
ADMIN <huydo164@gmail.com>
2021-02-25 09:41:15 +0700

    update

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -5,7 +5,6 @@ import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
 const _91ecc2f8 = () => interopDefault(import('..\\pages\\candidate\\index.vue' /* webpackChunkName: "pages/candidate/index" */))
-const _7ffac4f7 = () => interopDefault(import('..\\pages\\charts\\index.js' /* webpackChunkName: "pages/charts/index" */))
 const _fdeeaea2 = () => interopDefault(import('..\\pages\\Dashboard.vue' /* webpackChunkName: "pages/Dashboard" */))
 const _1b810613 = () => interopDefault(import('..\\pages\\history\\index.vue' /* webpackChunkName: "pages/history/index" */))
 const _b9ea0432 = () => interopDefault(import('..\\pages\\item.js' /* webpackChunkName: "pages/item" */))
@@ -13,28 +12,12 @@ const _c0ff16f8 = () => interopDefault(import('..\\pages\\Login.vue' /* webpackC
 const _23c825d8 = () => interopDefault(import('..\\pages\\Register.vue' /* webpackChunkName: "pages/Register" */))
 const _c2dd4208 = () => interopDefault(import('..\\pages\\templatemail\\index.vue' /* webpackChunkName: "pages/templatemail/index" */))
 const _800ca55a = () => interopDefault(import('..\\pages\\candidate\\add.vue' /* webpackChunkName: "pages/candidate/add" */))
-const _41b80d3c = () => interopDefault(import('..\\pages\\charts\\CChartBarExample.vue' /* webpackChunkName: "pages/charts/CChartBarExample" */))
-const _f1a80460 = () => interopDefault(import('..\\pages\\charts\\CChartBarSimple.vue' /* webpackChunkName: "pages/charts/CChartBarSimple" */))
-const _95a2451a = () => interopDefault(import('..\\pages\\charts\\CChartDoughnutExample.vue' /* webpackChunkName: "pages/charts/CChartDoughnutExample" */))
-const _78d6b87e = () => interopDefault(import('..\\pages\\charts\\CChartLineExample.vue' /* webpackChunkName: "pages/charts/CChartLineExample" */))
-const _59d002ab = () => interopDefault(import('..\\pages\\charts\\CChartLineSimple.vue' /* webpackChunkName: "pages/charts/CChartLineSimple" */))
-const _a8283bba = () => interopDefault(import('..\\pages\\charts\\CChartPieExample.vue' /* webpackChunkName: "pages/charts/CChartPieExample" */))
-const _3c110fa4 = () => interopDefault(import('..\\pages\\charts\\CChartPolarAreaExample.vue' /* webpackChunkName: "pages/charts/CChartPolarAreaExample" */))
-const _10945dae = () => interopDefault(import('..\\pages\\charts\\CChartRadarExample.vue' /* webpackChunkName: "pages/charts/CChartRadarExample" */))
-const _24c7411f = () => interopDefault(import('..\\pages\\charts\\Charts.vue' /* webpackChunkName: "pages/charts/Charts" */))
-const _35d51362 = () => interopDefault(import('..\\pages\\charts\\MainChartExample.vue' /* webpackChunkName: "pages/charts/MainChartExample" */))
-const _78721b5a = () => interopDefault(import('..\\pages\\notifications\\Alerts.vue' /* webpackChunkName: "pages/notifications/Alerts" */))
-const _466c5d73 = () => interopDefault(import('..\\pages\\notifications\\Badges.vue' /* webpackChunkName: "pages/notifications/Badges" */))
-const _a04256ae = () => interopDefault(import('..\\pages\\notifications\\Modals.vue' /* webpackChunkName: "pages/notifications/Modals" */))
 const _81e4dc8c = () => interopDefault(import('..\\pages\\pages\\Page404.vue' /* webpackChunkName: "pages/pages/Page404" */))
 const _18892212 = () => interopDefault(import('..\\pages\\pages\\Page500.vue' /* webpackChunkName: "pages/pages/Page500" */))
 const _56d596be = () => interopDefault(import('..\\pages\\sendmail\\MailInter.vue' /* webpackChunkName: "pages/sendmail/MailInter" */))
 const _2a8f2a21 = () => interopDefault(import('..\\pages\\sendmail\\MailOffer.vue' /* webpackChunkName: "pages/sendmail/MailOffer" */))
 const _33bd00a2 = () => interopDefault(import('..\\pages\\sendmail\\MailThank.vue' /* webpackChunkName: "pages/sendmail/MailThank" */))
 const _2ce72bcb = () => interopDefault(import('..\\pages\\templatemail\\add.vue' /* webpackChunkName: "pages/templatemail/add" */))
-const _46c2bd6e = () => interopDefault(import('..\\pages\\users\\User.vue' /* webpackChunkName: "pages/users/User" */))
-const _d57edd16 = () => interopDefault(import('..\\pages\\users\\Users.vue' /* webpackChunkName: "pages/users/Users" */))
-const _0cc321f0 = () => interopDefault(import('..\\pages\\users\\UsersData.js' /* webpackChunkName: "pages/users/UsersData" */))
 const _2c6d1e56 = () => interopDefault(import('..\\pages\\widgets\\Widgets.vue' /* webpackChunkName: "pages/widgets/Widgets" */))
 const _3edfa262 = () => interopDefault(import('..\\pages\\widgets\\WidgetsBrand.vue' /* webpackChunkName: "pages/widgets/WidgetsBrand" */))
 const _a39be8f4 = () => interopDefault(import('..\\pages\\widgets\\WidgetsDropdown.vue' /* webpackChunkName: "pages/widgets/WidgetsDropdown" */))
@@ -62,10 +45,6 @@ export const routerOptions = {
     path: "/candidate",
     component: _91ecc2f8,
     name: "candidate"
-  }, {
-    path: "/charts",
-    component: _7ffac4f7,
-    name: "charts"
   }, {
     path: "/Dashboard",
     component: _fdeeaea2,
@@ -95,58 +74,6 @@ export const routerOptions = {
     component: _800ca55a,
     name: "candidate-add"
   }, {
-    path: "/charts/CChartBarExample",
-    component: _41b80d3c,
-    name: "charts-CChartBarExample"
-  }, {
-    path: "/charts/CChartBarSimple",
-    component: _f1a80460,
-    name: "charts-CChartBarSimple"
-  }, {
-    path: "/charts/CChartDoughnutExample",
-    component: _95a2451a,
-    name: "charts-CChartDoughnutExample"
-  }, {
-    path: "/charts/CChartLineExample",
-    component: _78d6b87e,
-    name: "charts-CChartLineExample"
-  }, {
-    path: "/charts/CChartLineSimple",
-    component: _59d002ab,
-    name: "charts-CChartLineSimple"
-  }, {
-    path: "/charts/CChartPieExample",
-    component: _a8283bba,
-    name: "charts-CChartPieExample"
-  }, {
-    path: "/charts/CChartPolarAreaExample",
-    component: _3c110fa4,
-    name: "charts-CChartPolarAreaExample"
-  }, {
-    path: "/charts/CChartRadarExample",
-    component: _10945dae,
-    name: "charts-CChartRadarExample"
-  }, {
-    path: "/charts/Charts",
-    component: _24c7411f,
-    name: "charts-Charts"
-  }, {
-    path: "/charts/MainChartExample",
-    component: _35d51362,
-    name: "charts-MainChartExample"
-  }, {
-    path: "/notifications/Alerts",
-    component: _78721b5a,
-    name: "notifications-Alerts"
-  }, {
-    path: "/notifications/Badges",
-    component: _466c5d73,
-    name: "notifications-Badges"
-  }, {
-    path: "/notifications/Modals",
-    component: _a04256ae,
-    name: "notifications-Modals"
-  }, {
     path: "/pages/Page404",
     component: _81e4dc8c,
     name: "pages-Page404"
@@ -170,18 +97,6 @@ export const routerOptions = {
     path: "/templatemail/add",
     component: _2ce72bcb,
     name: "templatemail-add"
-  }, {
-    path: "/users/User",
-    component: _46c2bd6e,
-    name: "users-User"
-  }, {
-    path: "/users/Users",
-    component: _d57edd16,
-    name: "users-Users"
-  }, {
-    path: "/users/UsersData",
-    component: _0cc321f0,
-    name: "users-UsersData"
   }, {
     path: "/widgets/Widgets",
     component: _2c6d1e56,
